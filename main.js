0f5e8a55f3997c2fdae832cafb42dc828db1efd7
Ahmed-Samir444 <ahmedsamirr126@gmail.com>
2022-12-25 13:44:05 +0200

    add css file and modified

---
+++ main.js
@@ -0,0 +1,23 @@
+// create main div
+let container = document.createElement("div");
+container.id = "container";
+
+// styling for container
+container.style.cssText = "width: 80%; hight: 80vh; marg"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add container to body
+document.body.prepend(container)
